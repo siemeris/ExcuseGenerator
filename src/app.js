82725d236deaca0e4fdb878f5e2da3c89a22395f
siemeris <siemeris@gmail.com>
2022-07-01 14:25:15 +0000

    closing issues arranging code and DRY

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
 
+import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let indexGenerator = originArray => {
+  let Index = Math.floor(Math.random() * originArray.length);
+  return Index;
+};
+
+let excuseGenerator = () => {
+  let excuse =
+    who[indexGenerator(who)] +
+    " " +
+    action[indexGenerator(action)] +
+    " " +
+    what[indexGenerator(what)] +
+    " " +
+    when[indexGenerator(when)];
+
+  return excuse;
+};
 
 window.onload = () => {
   //write your code here
@@ -11,33 +40,4 @@ window.onload = () => {
     document.querySelector("#excuse").innerHTML = excuseGenerator();
   });
   console.log(`Hello Rigo from the console!`);
-};
-
-let excuseGenerator = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
-
-  let excuse =
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex];
-
-  return excuse;
 };
